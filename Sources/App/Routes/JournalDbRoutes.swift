da59d7705048f0119652fc7c94c2b5a4baef49e1
Piotr <piotr@fastmail.co.uk>
2020-05-10 21:34:21 +0100

    api works

--- Sources/App/Routes/JournalDbRoutes.swift
+++ Sources/App/Routes/JournalDbRoutes.swift
@@ -4,7 +4,7 @@ struct JournalDbRoutes: RouteCollection {
     
     
     func boot(router: Router) throws {
-        let topRouter = router.grouped("api", "journal")
+        let topRouter = router.grouped("/api")
         topRouter.get(use: getAll)
         topRouter.post(use: newEntry)
         
@@ -12,19 +12,20 @@ struct JournalDbRoutes: RouteCollection {
     
     
     
-    func getAll(_ req: Request) -> Future<[JournalEntry]> {
+    func getAll(_ req: Request) throws -> Future<[JournalEntry]> {
         
         return JournalEntry.query(on: req).all()
         }
     
     
     func newEntry(_ req: Request) throws -> Future<JournalEntry> {
-        return try req.content.decode(JournalEntry.self).flatMap { entry in
+        return try req.content.decode(JournalEntry.self).flatMap(to: JournalEntry.self) { entry in
             return entry.save(on: req)
+        }
         }
 //        return try req.content.decode(JournalEntry.self).save(on: req)
         
-    }
+    
     
 }
 
